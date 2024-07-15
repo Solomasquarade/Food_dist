f70ef3d016742252fd65fbe39abb545931480665
Solomasquarade <104280380+Solomasquarade@users.noreply.github.com>
2024-07-15 12:02:19 +0300

    добавлен rest оператор в классы

--- js/script.js
+++ js/script.js
@@ -149,12 +149,13 @@ window.addEventListener('scroll', showModalByScroll);
 
 //Используем классы для карточек
 class MenuCard {
-    constructor (src, alt, title, descr, price, parentSelector) {
+    constructor (src, alt, title, descr, price, parentSelector, ...classes) {
         this.src = src;
         this.alt = alt;
         this.title = title;
         this.descr = descr;
         this.price = price;
+        this.classes = classes;
         this.parent = document.querySelector(parentSelector);
         this.transfer = 27;
         this.changeToUAH();
@@ -165,7 +166,12 @@ class MenuCard {
     }
     render() {
         const element = document.createElement('div');
-        element.innerHTML = `<div class="menu__item">
+        if (this.classes.length === 0)
+        {this.element = 'menu__item';
+          element.classList.add(this.element);
+        } else {this.classes.forEach(className =>element.classList.add(className));}
+        
+        element.innerHTML = `
         <img src=${this.src} alt=${this.alt}>
         <h3 class="menu__item-subtitle">${this.title}</h3>
         <div class="menu__item-descr">${this.descr}</div>
@@ -174,7 +180,7 @@ class MenuCard {
             <div class="menu__item-cost">Цена:</div>
             <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
         </div>
-    </div>`; 
+  `; 
     this.parent.append(element);
     }
 }
@@ -185,7 +191,8 @@ new MenuCard(
     'Меню "Фитнес"',
     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
     9,
-    ".menu .container"
+    ".menu .container",
+    'menu__item'
 ).render();
 
 new MenuCard(
@@ -194,7 +201,9 @@ new MenuCard(
     'Меню "Постное"',
     'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
     14,
-    ".menu .container"
+    ".menu .container",
+    'menu__item',
+    
 ).render();
 
 new MenuCard(
@@ -203,7 +212,8 @@ new MenuCard(
     'Меню “Премиум”',
     'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
     21,
-    ".menu .container"
+    ".menu .container",
+    'menu__item'
 ).render();
 });
 
