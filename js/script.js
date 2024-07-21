c6b15d08a55697a5b91b44c5fd19da56c1e1f2f3
Solomasquarade <104280380+Solomasquarade@users.noreply.github.com>
2024-07-21 13:51:23 +0300

    добавлен слайдер на нативном js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 window.addEventListener('DOMContentLoaded', function() {
 
     // Tabs
+    //  http://localhost:8888/   В МАМП preferences
+    //  npx json-server db.json запуск сервера
     
 	let tabs = document.querySelectorAll('.tabheader__item'),
 		tabsContent = document.querySelectorAll('.tabcontent'),
@@ -40,8 +42,7 @@ window.addEventListener('DOMContentLoaded', function() {
     });
     
     // Timer
-
-    const deadline = '2024-18-07';
+    const deadline = '2024-07-23';
 
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -190,15 +191,19 @@ window.addEventListener('DOMContentLoaded', function() {
         return await res.json();
   };
  
-  getResource('http://localhost:3000/menu')
-        .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
-                new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
-            });
-        });
+//   getResource('http://localhost:3000/menu')
+//         .then(data => {
+            // data.forEach(({img, altimg, title, descr, price}) => {
+            //     new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+            // });
+//         });
 // Получаем массив Меню с объектами, перебираем через фор ич, объект внутри деструктуризируется, части передаем в конструктор,рендерим 
     
-
+axios.get('http://localhost:3000/menu')
+      .then(data => {data.data.forEach(({img, altimg, title, descr, price}) => {
+        new MenuCard(img, altimg, title, descr, price, ".menu .container").render();
+    });
+});
     // Forms
 
     const forms = document.querySelectorAll('form');
@@ -278,8 +283,51 @@ const postData = async(url,data) => {
             closeModal();
         }, 4000);
     }
-fetch('http://localhost:3000/menu')
-.then(data => data.json())
-.then(res => console.log(res));
 
+    //slider
+    let slideIndex = 1;
+    const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current');
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+    } else {
+        total.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block'; // Как ваша самостоятельная работа - переписать на использование классов show/hide
+        
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+    }
+
+    function plusSlides (n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', function(){
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function(){
+        plusSlides(1);
+    });
 });
