ea0e7a1b89bfdcb7ab9749983e6041bcfdd41bfd
Solomasquarade <104280380+Solomasquarade@users.noreply.github.com>
2024-08-05 17:29:48 +0300

    fixing bugs with modal window

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
+import tabs  from'./modules/tabs';
+import modal  from'./modules/modal';
+import timer  from'./modules/timer';
+import cards  from'./modules/cards';
+import calc  from'./modules/calc';
+import forms  from'./modules/forms';
+import slider  from'./modules/slider';
+
 window.addEventListener('DOMContentLoaded', function() {
         //  http://localhost:8888/   В МАМП preferences
     //  npx json-server db.json запуск сервера
-    const tabs = require('./modules/tabs'),
-    modal = require('./modules/modal'),
-    timer = require('./modules/timer'),
-    cards = require('./modules/cards'),
-    calc = require('./modules/calc'),
-    forms = require('./modules/forms'),
-    slider = require('./modules/slider');
+
 
 tabs();
 modal();
